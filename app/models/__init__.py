0add792e82a05c3f70cdb5e4709e801f28e0b280
tantran <tantran.dev2101@gmail.com>
2024-04-30 21:51:09 +0700

    feat(app): update model and migration (pending)

---
+++ app/models/__init__.py
@@ -0,0 +1,7 @@
+from .user import User
+from .apply import Apply
+from .company import Company
+from .hr_company import Hr_company
+from .hr_register import Hr_register
+from .job import Job
+from .carrer import Carrer
