49d1524f5b861853a75ae955ab0b3239cd122d96
tantran <tantran.dev2101@gmail.com>
2024-05-12 03:29:05 +0700

    update code

--- career/asgi.py
+++ career/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'carrer.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'career.settings')
 
 application = get_asgi_application()
