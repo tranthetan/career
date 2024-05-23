37e6dc7ad9a24825253b3d0e7d181e3220702149
tantran <tantran@vietop.edu.vn>
2024-05-23 20:10:08 +0700

    update migrate and model

--- app/models/job.py
+++ app/models/job.py
@@ -2,12 +2,13 @@ from django.db import models
 from .user import User
 from .company import Company
 from .career import Career
+from .work_type import WorkType
 
 class Job(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     salary_range = models.CharField(max_length=255)
-    work_type = models.IntegerField()
+    work_type = models.ForeignKey(WorkType, on_delete=models.CASCADE)
     creator = models.ForeignKey(User, related_name='job_creations', on_delete=models.CASCADE)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     career = models.ForeignKey(Career, on_delete=models.CASCADE)
