df14efb5e2a776a0746788ca83f0cc048a7846bd
tantran <tantran@vietop.edu.vn>
2024-05-25 15:37:07 +0700

    fix dropdown item authen

--- app/views/auth.py
+++ app/views/auth.py
@@ -24,4 +24,4 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     messages.info(request, "You have successfully logged out.")
-    return redirect('login')
+    return redirect(reverse('home'))
