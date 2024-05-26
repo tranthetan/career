ddeca87502f5e1e65041669241be21b1b664b7b4
hieu99dev <hoangkimhieu99dev@gmail.com>
2024-05-26 22:07:54 +0700

    update job

--- app/admin_urls.py
+++ app/admin_urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('logout/', auth.admin_logout, name='admin-logout'),
     path('company/', auth.company, name='admin-company'),
     path('jobs/', job.get_jobs, name='admin-jobs'),
+    path('jobs/edit/<int:job_id>/', job.edit_job, name='admin-edit-job'),
     path('hr/applies', job.get_applies, name='applies'),
     path('requests', request.requests, name='requests'),
     path('request/<int:request_id>/approve-or-reject', request.action_approve_or_reject, name='approve-or-reject'),
