f2e233b64ac7cc5756cc9210f08043826811ce59
tantran <tantran@vietop.edu.vn>
2024-05-27 21:26:02 +0700

    update upload cv

--- app/views/job.py
+++ app/views/job.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
 from ..models import Job, Apply
 from django.core.files.storage import default_storage
+from django.contrib import messages
+from django.shortcuts import render, redirect, reverse
 
 def show(request, job_id):
     job = Job.objects.get(id = job_id)
@@ -20,7 +21,7 @@ def apply(request, job_id):
             resume = request.FILES.get('resume')
             file_name = default_storage.save(f'resume/{resume.name}', resume)
             Apply.objects.create(
-                hr_proces = job.creator_id,
+                hr_process = job.creator_id,
                 cover_letter = cover_letter,
                 resume_path = default_storage.url(file_name),
                 status = 0,
@@ -31,4 +32,4 @@ def apply(request, job_id):
             messages.success(request, 'Apply successfully!')
         else:
             messages.success(request, 'Apply failed!')
-    return redirect(reverse('job-detail', args=[job_id]))
+    return redirect(reverse('home'))
