5f27d4577463b5e8e1c884fedb5600960ef4f08f
tantran <tantran@vietop.edu.vn>
2024-05-27 21:12:02 +0700

    fix upload resume

--- app/views/job.py
+++ app/views/job.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from ..models import Job, Apply
+from django.core.files.storage import default_storage
 
 def show(request, job_id):
     job = Job.objects.get(id = job_id)
@@ -17,11 +18,11 @@ def apply(request, job_id):
         if job:
             cover_letter = request.POST.get('cover_letter')
             resume = request.FILES.get('resume')
-            resume_path = default_storage.save(f'resume/{resume.name}', resume)
+            file_name = default_storage.save(f'resume/{resume.name}', resume)
             Apply.objects.create(
                 hr_proces = job.creator_id,
                 cover_letter = cover_letter,
-                resume_path = default_storage.url(resume_path),
+                resume_path = default_storage.url(file_name),
                 status = 0,
                 user_id = user.id,
                 company_id = job.company_id,
